df27db3479affb2cba8b122620eebeaa292075ec
darrenlooby <darren.looby@googlemail.com>
2018-02-07 13:52:21 +0000

    Passing redis in

--- index.js
+++ index.js
@@ -28,66 +28,76 @@ const retry_strategy = function (options) {
     return Math.min(options.attempt * 100, 3000);
 };
 
-// create a redis connections
-const redisConfig = {
-  cms: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    db: process.env.REDIS_DB,
-    retry_strategy: retry_strategy
-  },
-  users: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    db: process.env.REDIS_USER_DB,
-    retry_strategy: retry_strategy
+function load (redisConfig) {
+  // Set up all required redis connections
+  if (typeof redisConfig.cms.retry_strategy === 'undefined') {
+    redisConfig.cms.retry_strategy = retry_strategy;
   }
-};
+  let cms = redis.createClient(redisConfig.cms);
+  if (typeof redisConfig.users.retry_strategy === 'undefined') {
+    redisConfig.users.retry_strategy = retry_strategy;
+  }
+  let users = redis.createClient(redisConfig.users);
 
-// Set up all required redis connections
-let cms = redis.createClient(redisConfig.cms);
-let users = redis.createClient(redisConfig.users);
+  // show error in console
+  cms.on("error", function (err) {
+      console.log("CMS Error " + err);
+  });
 
-// show error in console
-cms.on("error", function (err) {
-    console.log("CMS Error " + err);
-});
+  // show error in console
+  users.on("error", function (err) {
+      console.log("User Error " + err);
+  });
 
-// show error in console
-users.on("error", function (err) {
-    console.log("User Error " + err);
-});
+  // Begin the intial app
+  let app = express();
 
-// Begin the intial app
-let app = express();
+  // Ensure that body and cookies can be used
+  app.use(bodyParser.json()); // support json encoded bodies
+  app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+  app.use(cookieParser());
 
-// Ensure that body and cookies can be used
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-app.use(cookieParser());
+  // Do security stuff
 
-// Do security stuff
-app.use(helmet());
+  // Implement X-XSS-Protection
+  app.use(helmet.xssFilter());
 
-// To support local development, provide a local CDN
-if (typeof process.env.CDN !== 'undefined') {
-  const serveIndex = require('serve-index');
-  app.use('/public', express.static(`${process.cwd()}${process.env.CDN}`), serveIndex(`${process.cwd()}/cdn`, {'icons': true}));
+  // Implement X-Frame: Deny
+  app.use(helmet.frameguard());
+
+  // Implement Strict-Transport-Security
+  app.use(helmet.hsts({
+    maxAge: 7776000000,
+    includeSubdomains: true
+  }));
+
+  // Hide X-Powered-By
+  app.use(helmet.hidePoweredBy());
+
+  // To support local development, provide a local CDN
+  if (typeof process.env.CDN !== 'undefined') {
+    const serveIndex = require('serve-index');
+    console.log('cdn root', `${process.cwd()}${process.env.CDN}`);
+    app.use('/public', express.static(`${process.cwd()}/${process.env.CDN}`), serveIndex(`${process.cwd()}/${process.env.CDN}`, {'icons': true}));
+  }
+
+  // Attach all the application middleware
+  const permitCheck = require('./libs/permitCheck');
+  const siteEnabledCheck = require('./libs/siteEnabledCheck')(cms);
+  const formPostProcessor = require('./libs/formPostProcessor');
+  const loginformProcessor = require('./libs/loginformProcessor')(users);
+  const logoutProcessor = require('./libs/logoutProcessor');
+  const generatePage = require('./libs/generatePage')(cms);
+
+  app.use(permitCheck);
+  app.use(siteEnabledCheck);
+  app.post('/form', formPostProcessor);
+  app.post('/login', loginformProcessor);
+  app.all('/logout', logoutProcessor);
+  app.use(generatePage);
+
+  return app;
+
 }
 
-// Attach all the application middleware
-const permitCheck = require('./libs/permitCheck');
-const siteEnabledCheck = require('./libs/siteEnabledCheck')(cms);
-const formPostProcessor = require('./libs/formPostProcessor');
-const loginformProcessor = require('./libs/loginformProcessor')(users);
-const logoutProcessor = require('./libs/logoutProcessor');
-const generatePage = require('./libs/generatePage')(cms);
-
-app.use(permitCheck);
-app.use(siteEnabledCheck);
-app.post('/form', formPostProcessor);
-app.post('/login', loginformProcessor);
-app.all('/logout', logoutProcessor);
-app.use(generatePage);
-
-module.exports = app;
+module.exports = load;
